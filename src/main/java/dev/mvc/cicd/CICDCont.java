8e3874eb2f13af62d10f6d69d313f808e9287418
commony0 <nawoony98@gmail.com>
2023-12-13 12:16:10 +0900

    workflow 정상 테스트

--- src/main/java/dev/mvc/cicd/CICDCont.java
+++ src/main/java/dev/mvc/cicd/CICDCont.java
@@ -17,14 +17,17 @@ public class CICDCont {
     String msg = "";
     
     if (sw == -1) {
-      msg = "/cicd 호출됨, error 로그";
+      msg = "/cide 호출됨, error 로그";
       log.error(msg);
-    } else if (sw == 1) {
-      msg = "/cide 호출됨, info 로그";
-      log.info(msg);
     } else if (sw == 0) {
       msg = "/cide 호출됨, warn 로그";
       log.warn(msg);
+    } else if (sw == 1) {
+      msg = "/cicd 호출됨, info 로그";
+      log.info(msg);
+    } else if (sw == 2) {
+      msg = "/cide workflow 테스트";
+      log.info(msg);
     }
     
     return "<h3>" + msg + "</h3>";
